df0fc3c2cb54623ea3d1796bc1dc8835e474da63
tadabaseChem <122552112+tadabaseChem@users.noreply.github.com>
2023-01-12 13:20:59 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-window.prod.mjs');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-sw.js');
 
 workbox.routing.registerRoute(
     ({request}) => request.destination === 'image',
